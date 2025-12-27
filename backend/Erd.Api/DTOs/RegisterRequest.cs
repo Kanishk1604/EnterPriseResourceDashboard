1d1a5342564235acfc26d5747717ee113d56a393
Kanishk1604 <kanishksaini1604@gmail.com>
2025-12-27 01:22:52 -0500

    Created DTOs

---
+++ backend/Erd.Api/DTOs/RegisterRequest.cs
@@ -0,0 +1,9 @@
+namespace Erd.Api.DTOs;
+
+public class RegisterRequest
+{
+    public string FullName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public int RoleId { get; set; }
+}
